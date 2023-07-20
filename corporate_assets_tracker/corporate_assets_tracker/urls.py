da4a1ce4d8358a37b574fe4a81f086f3bc2b0804
Adnanrobi <adnanrobi6@gmail.com>
2023-07-21 00:08:55 +0600

    Automated API Documentation

--- corporate_assets_tracker/corporate_assets_tracker/urls.py
+++ corporate_assets_tracker/corporate_assets_tracker/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title="API Documentation")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('companies/', include('companies.urls')),
-    path('employees/', include('employees.urls')),
-    path('devices/', include('devices.urls')),
-    path('api/', include('devices.api_urls')),  # Include the API URLs from the 'devices' app
+    path("admin/", admin.site.urls),
+    path("api/docs/", schema_view),
+    path("companies/", include("companies.urls")),
+    path("employees/", include("employees.urls")),
+    path("devices/", include("devices.urls")),
+    path(
+        "api/", include("devices.api_urls")
+    ),  # Include the API URLs from the 'devices' app
 ]
-
