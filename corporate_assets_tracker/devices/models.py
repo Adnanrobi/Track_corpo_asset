112a3e6fe6165e1c39809a09bd8035b680189cee
Adnanrobi <adnanrobi6@gmail.com>
2023-07-20 22:34:15 +0600

    defined different models

---
+++ corporate_assets_tracker/devices/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Device(models.Model):
+    DEVICE_TYPES = [
+        ("phone", "Phone"),
+        ("tablet", "Tablet"),
+        ("laptop", "Laptop"),
+        # Add other types here
+    ]
+
+    device_type = models.CharField(choices=DEVICE_TYPES, max_length=10)
+    model = models.CharField(max_length=100)
+    serial_number = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f"{self.device_type} - {self.model} ({self.serial_number})"
